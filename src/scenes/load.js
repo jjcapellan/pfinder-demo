31e96cbff7a94889b0c18646d7484dd25c86edfe
jjcapellan <soycape@hotmail.com>
2023-01-22 00:55:01 +0100

    Deleted not used variables

--- src/scenes/load.js
+++ src/scenes/load.js
@@ -12,32 +12,31 @@ export default class Load extends Phaser.Scene {
     preload() {
 
         const items = 3;
-        const mapWidth = Math.floor(this.game.config.width / TILESIZE);
-        const mapHeight = Math.floor(this.game.config.height / TILESIZE);
 
         this.textures.on('addtexture', () => {
             count++;
             if (count == items) this.scene.start('menu');
         });
-        this.generateMap(mapWidth, mapHeight);
+        
+        this.generateMap();
         this.generateRectTexture(TILESIZE / 2, TILESIZE / 2, 'walkertexture', COLOR_PLAYER); // player
         this.generateRectTexture(TILESIZE / 2, TILESIZE / 2, 'npctexture', COLOR_NPC); // npc
         this.generateRectTexture(4, 4, 'backtexture', COLOR_BACKGROUND); // text background
 
     }
 
-    generateMap(width, height) {
+    generateMap() {
         const map = map137x76;
 
         let graphics = this.add.graphics();
         graphics.fillStyle(COLOR_FOREGROUND, 1);
-        for (let y = 0; y < height; y++) {
+        for (let y = 0; y < map.length; y++) {
             let row = map[y];
             row.forEach((_, x) => {
                 if (row[x]) graphics.fillRect(TILESIZE * x, TILESIZE * y, TILESIZE, TILESIZE);
             });
         }
-        graphics.generateTexture('maptexture', width * TILESIZE, height * TILESIZE);
+        graphics.generateTexture('maptexture', map[0].length * TILESIZE, map.length * TILESIZE);
 
     }
 
